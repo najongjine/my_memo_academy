e06062fea0e5c8fc5866f878c7b8aa1316c8a49c
najongjine <najongjin3@hotmail.com>
2025-06-12 11:08:54 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const text2 = "쓰레기값";
   const [text, setText] = useState<string>("쓰레기값");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" value={text} />
+        <input type="text" value={text} onChange={handleChange} />
         <div>당신이 입력한 값:{text}</div>
       </header>
     </div>
