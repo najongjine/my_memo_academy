02fc63154d0acef4f17f157bbcd0f3ba9105fe7a
najongjine <najongjin3@hotmail.com>
2025-06-20 10:40:09 +0900

    .

--- src/pages/MemoEditor.tsx
+++ src/pages/MemoEditor.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 
 const MemoEditor: React.FC = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <div>
       <div>메모 작성</div>
+      <input
+        type="text"
+        placeholder="제목을 입력하세요"
+        value={title}
+        onChange={(e) => setTitle(e?.target?.value ?? "")}
+      />
     </div>
   );
 };
