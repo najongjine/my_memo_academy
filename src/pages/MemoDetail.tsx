4a8cf9962c25ff57851d70be88dc117907bf53d7
najongjine <najongjin3@hotmail.com>
2025-06-24 11:14:16 +0900

    .

---
+++ src/pages/MemoDetail.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const MemoDetail: React.FC = () => {
+  return (
+    <div className="max-w-md mx-auto mt-5 p-4 bg-white rounded shadow">
+      <h2 className="text-xl font-bold mb-4">📝 메모 상세 페이지입니다!</h2>
+    </div>
+  );
+};
+
+export default MemoDetail;
