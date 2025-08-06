65cdb1b64838a18ac85488d697e9b7ed22a70872
najongjine <najongjin3@hotmail.com>
2025-08-06 13:54:27 +0900

    .

--- src/pages/FileUploadTest.tsx
+++ src/pages/FileUploadTest.tsx
@@ -6,12 +6,44 @@ import { constants } from "fs/promises";
 
 
 const FileUploadTest: React.FC = () => {
-
+  const [files, setFiles] = useState<FileList | null>(null);
   const [title, setTitle] = useState<string>("")
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFiles(e?.target?.files);
+  }
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault(); // 꼭 필요
+
+    if (!files?.length) {
+      alert("파일을 선택해 주세요!");
+      return;
+    }
+    /*
+    formdata 는 파일첨부는 해줄수 있는 객체이다.
+    파일전송은 무조건 formdata 로 해야된다
+     */
+    const formData = new FormData();
+    formData.append("title", title);
+    Array.from(files).forEach(
+      (file) => {
+        // 파일 첨부
+        formData.append("images", file);
+      }
+    );
+
+    try {
+      let response = await axios.post(`http://localhost:3001/api/file/local_upload`
+        , formData
+        , {
+          headers: { "Content-Type": "multipart/form-data" }
+        }
+      )
+    } catch (error: any) {
+      console.error(`!! server error. ${error?.message ?? ""}`);
+      alert(`!! server error. ${error?.message ?? ""}`);
+    }
 
   }
 
